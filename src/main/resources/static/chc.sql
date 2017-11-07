f5a6b0b8abdd4c561f0e4546efb25e3bbe95e3ff
shinyzo <11742979@qq.com>
2017-11-07 23:19:28 +0800

    SQL

--- src/main/resources/static/chc.sql
+++ src/main/resources/static/chc.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-11-06 00:59:31
+Date: 2017-11-07 23:18:39
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -37,6 +37,25 @@ INSERT INTO `mobile_nav` VALUES ('N000003', '诊单', '/bill', 'icon-stack', '3'
 INSERT INTO `mobile_nav` VALUES ('N000004', '个人中心', '/owner', 'icon-compass2', '4');
 
 -- ----------------------------
+-- Table structure for `role_nav`
+-- ----------------------------
+DROP TABLE IF EXISTS `role_nav`;
+CREATE TABLE `role_nav` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` varchar(12) NOT NULL,
+  `nav_id` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of role_nav
+-- ----------------------------
+INSERT INTO `role_nav` VALUES ('1', '1', 'N000001');
+INSERT INTO `role_nav` VALUES ('2', '1', 'N000002');
+INSERT INTO `role_nav` VALUES ('3', '1', 'N000003');
+INSERT INTO `role_nav` VALUES ('4', '1', 'N000004');
+
+-- ----------------------------
 -- Table structure for `user_info`
 -- ----------------------------
 DROP TABLE IF EXISTS `user_info`;
@@ -49,6 +68,7 @@ CREATE TABLE `user_info` (
   `mobile_no` varchar(11) DEFAULT NULL,
   `openid` varchar(32) NOT NULL,
   `usertype` varchar(4) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
   `user_status` varchar(4) DEFAULT NULL,
   `login_count` int(10) DEFAULT NULL,
   `last_login_ip` varchar(16) DEFAULT NULL,
@@ -62,24 +82,5 @@ CREATE TABLE `user_info` (
 -- ----------------------------
 -- Records of user_info
 -- ----------------------------
-INSERT INTO `user_info` VALUES ('1', 'shinyzo', '123456', null, null, '15501700742', '123456789', '1', null, '1', '127.0.0.1', '2017-11-04 04:54:03');
-INSERT INTO `user_info` VALUES ('2', 'shinyzo1', '1111', null, null, '15501700743', '123456787', '2', null, '2', '127.0.0.1', '2017-11-04 05:13:21');
-
--- ----------------------------
--- Table structure for `user_nav`
--- ----------------------------
-DROP TABLE IF EXISTS `user_nav`;
-CREATE TABLE `user_nav` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `usertype` varchar(12) NOT NULL,
-  `nav_id` varchar(32) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of user_nav
--- ----------------------------
-INSERT INTO `user_nav` VALUES ('1', '1', 'N000001');
-INSERT INTO `user_nav` VALUES ('2', '1', 'N000002');
-INSERT INTO `user_nav` VALUES ('3', '1', 'N000003');
-INSERT INTO `user_nav` VALUES ('4', '1', 'N000004');
+INSERT INTO `user_info` VALUES ('1', 'shinyzo', '123456', null, null, '15501700742', '123456789', '1', '1', '1', '1', '127.0.0.1', '2017-11-07 22:41:31');
+INSERT INTO `user_info` VALUES ('2', 'shinyzo1', '1111', null, null, '15501700743', '123456787', '2', '2', '1', '2', '127.0.0.1', '2017-11-07 22:41:32');
